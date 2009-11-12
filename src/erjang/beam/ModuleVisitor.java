6101af4f801fc52a41489f7382dc6df827392cd0
Kresten Krab Thorup <krab@trifork.com>
2009-11-12 17:00:03 +0100

    some more jvm codegen

---
+++ src/erjang/beam/ModuleVisitor.java
@@ -0,0 +1,42 @@
+/**
+ * This file is part of Erjang - A JVM-based Erlang VM
+ *
+ * Copyright (c) 2009 by Trifork
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *  
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ **/
+
+package erjang.beam;
+
+import erjang.EAtom;
+import erjang.EObject;
+
+/**
+ * 
+ */
+public interface ModuleVisitor {
+
+	void visitModule(EAtom name);
+
+	/** list of {Fun,Arity,Entry} */
+	void visitExport(EAtom fun, int arity, int entry);
+
+	/** list of {Atom,Value} */
+	void visitAttribute(EAtom att, EObject value);
+
+	/** Visit function */
+	FunctionVisitor visitFunction(EAtom name, int arity, int startLabel);
+
+	void visitEnd();
+
+}
