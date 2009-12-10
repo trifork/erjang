04370eeee697daae2c4faa558af0483cb9b03456
Kresten Krab Thorup <krab@trifork.com>
2009-12-10 17:18:27 +0100

    bootstrap error_logger for use until the real one comes up (meybe delete again after bootstrap complete)

---
+++ src/main/java/erjang/EErrorLogger.java
@@ -0,0 +1,45 @@
+/**
+ * This file is part of Erjang - A JVM-based Erlang VM
+ *
+ * Copyright (c) 2009 by Trifork
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *  
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ **/
+
+
+package erjang;
+
+import java.io.File;
+import java.net.MalformedURLException;
+
+/**
+ * 
+ */
+public class EErrorLogger {
+
+
+	public static void start()
+	{
+		try {
+			EModule.load_module(EAtom.intern("erjang"), new File("target/classes").toURI().toURL());
+		} catch (MalformedURLException e) {
+			throw new Error(e);
+		}
+		
+		EProc proc = new EProc(null, EAtom.intern("erjang"), EAtom.intern("error_loop"), ERT.NIL);
+		ERT.register(EAtom.intern("error_logger"), proc.self());
+		ERT.run(proc);		
+	}
+
+	
+}
