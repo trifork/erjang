d175a5ff0751348e25a447d5b5fff996f7d9400a
Kresten Krab Thorup <krab@trifork.com>
2009-12-24 14:02:26 +0100

    rewrite boot_otp to multiple lines

--- boot_otp.sh
+++ boot_otp.sh
@@ -1,1 +1,12 @@
-java -ea -cp erjang-0.1.jar -Derjpath=/sw/lib/erlang/lib/erts-5.7.3/ebin  erjang.OTPMain -boot start -root /sw/lib/erlang -init_debug -loader_debug
+
+java -ea -cp erjang-0.1.jar \
+    -Derjpath=/sw/lib/erlang/lib/erts-5.7.3/ebin erjang.OTPMain \
+    /sw/lib/erlang/erts-5.7.3/bin/erl \
+    -- \
+    -boot start \
+    -root /sw/lib/erlang \
+    -progname erl \
+    -home $HOME \
+    -init_debug \
+    -loader_debug
+
