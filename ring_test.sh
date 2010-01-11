3c14fee37a2fe709cecd7b49b01c0b0e3148c278
Kresten Krab Thorup <krab@trifork.com>
2010-01-11 11:04:13 +0100

    add -d64 to ring test script

--- ring_test.sh
+++ ring_test.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-java -cp erjang-0.1.jar -Derjpath=./src/main/erl/preloaded/ebin:src/main/erl erjang.Erj ring:main
+java -server -d64 -da -cp erjang-0.1.jar -Derjpath=./src/main/erl/preloaded/ebin:src/main/erl erjang.Erj ring:main
