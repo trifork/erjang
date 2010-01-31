3102436462577ef9ce9e1d475f92083db422def4
Erik Søe Sørensen <erik+gh@nogetmed.dk>
2010-01-31 14:58:33 +0100

    Moved symbolic-code related atom constants into a common place.

---
+++ src/main/java/erjang/beam/CodeAtoms.java
@@ -0,0 +1,23 @@
+package erjang.beam;
+import erjang.EAtom;
+
+public class CodeAtoms {
+	public static final EAtom TRUE_ATOM = EAtom.intern("true");
+	public static final EAtom FALSE_ATOM = EAtom.intern("false");
+	public static final EAtom X_ATOM = EAtom.intern("x");
+	public static final EAtom Y_ATOM = EAtom.intern("y");
+	public static final EAtom FR_ATOM = EAtom.intern("fr");
+	public static final EAtom NIL_ATOM = EAtom.intern("nil");
+	public static final EAtom INTEGER_ATOM = EAtom.intern("integer");
+	public static final EAtom STRING_ATOM = EAtom.intern("string");
+	public static final EAtom FLOAT_ATOM = EAtom.intern("float");
+	public static final EAtom ATOM_ATOM = EAtom.intern("atom");
+	public static final EAtom LITERAL_ATOM = EAtom.intern("literal");
+	public static final EAtom NOFAIL_ATOM = EAtom.intern("nofail");
+	public static final EAtom F_ATOM = EAtom.intern("f");
+	public static final EAtom FIELD_FLAGS_ATOM = EAtom.intern("field_flags");
+	public static final EAtom EXTFUNC_ATOM = EAtom.intern("extfunc");
+	public static final EAtom APPLY_ATOM = EAtom.intern("apply");
+	public static final EAtom ERLANG_ATOM = EAtom.intern("erlang");
+	public static final EAtom ERROR_ATOM = EAtom.intern("error");
+}
