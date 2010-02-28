6773aa0c09dae3f6cd29230e0e0d7c004f651c71
Kresten Krab Thorup <krab@trifork.com>
2010-02-28 13:45:09 +0100

    in run_erl_tests always erlc first

--- run_erl_tests.sh
+++ run_erl_tests.sh
@@ -1,9 +1,11 @@
 #!/bin/sh
 
+(cd src/test/erl; erlc *.erl)
+
 CMD=""
 for f in src/test/erl/*_tests.beam; do 
     test=`basename $f .beam`
-    CMD="$CMD -run $test test"
+    CMD="$CMD -s $test test"
 done
 
-./erl.sh -noshell -pa ./src/test/erl $CMD -run erlang halt
+./erl.sh -noshell -pa ./src/test/erl $CMD -s erlang halt
