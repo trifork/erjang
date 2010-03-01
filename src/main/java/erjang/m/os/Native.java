d5fc6a23c6706cf74477bf137aa73df11cbe4c15
Erik Søe Sørensen <erik+gh@nogetmed.dk>
2010-03-02 00:55:06 +0100

    os.type/0: A bit more detailed.

--- src/main/java/erjang/m/os/Native.java
+++ src/main/java/erjang/m/os/Native.java
@@ -37,10 +37,20 @@ public class Native extends ENative {
 	/**
 	 * 
 	 */
-	private static final EAtom am_macosx = EAtom.intern("macosx");
 	private static final EAtom am_unix = EAtom.intern("unix");
+	private static final EAtom am_win32 = EAtom.intern("win32");
 
-	private static final ETuple2 OS_TYPE = new ETuple2(am_unix, am_macosx);
+	private static final EAtom am_macosx = EAtom.intern("macosx");
+	private static final EAtom am_linux = EAtom.intern("linux");
+	private static final EAtom am_sunos = EAtom.intern("sunos");
+	private static final EAtom am_windows = EAtom.intern("windows");
+	private static final EAtom am_nt = EAtom.intern("nt");
+
+	private static final ETuple2 OS_TYPE_MACOS   = new ETuple2(am_unix,  am_macosx);
+	private static final ETuple2 OS_TYPE_LINUX   = new ETuple2(am_unix,  am_linux);
+	private static final ETuple2 OS_TYPE_SUNOS   = new ETuple2(am_unix,  am_sunos);
+	private static final ETuple2 OS_TYPE_WINDOWS = new ETuple2(am_win32, am_windows);
+	private static final ETuple2 OS_TYPE_WINNT   = new ETuple2(am_win32, am_nt);
 
 	/** os:getenv(string()) -> string() | false */
 	@BIF
@@ -59,6 +69,21 @@ public class Native extends ENative {
 	
 	@BIF
 	public static ETuple2 type() {
-		return OS_TYPE;
+	    String osname = System.getProperty("os.name");
+		/* TODO: On unices, the second element should always be lowercase of `uname -s`.
+		 * For now, we recognize a fixed set. */
+		if ("Mac OS".equals(osname) ||
+			"Mac OS X".equals(osname)) return OS_TYPE_MACOS;
+		if ("Linux".equals(osname)) return OS_TYPE_LINUX;
+		if ("Solaris".equals(osname) ||
+			"SunOS".equals(osname)) return OS_TYPE_SUNOS;
+		if ("Windows 95".equals(osname) ||
+			"Windows 98".equals(osname) ||
+			"Windows Me".equals(osname)) return OS_TYPE_WINDOWS;
+		if ("Windows NT".equals(osname) ||
+			"Windows 2000".equals(osname) ||
+			"Windows XP".equals(osname)) return OS_TYPE_WINNT;
+		// Fallback:
+		return OS_TYPE_MACOS;
 	}
 }
