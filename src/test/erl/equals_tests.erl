b6a3f915150da2e54392fd5772de4b238df89e87
Kresten Krab Thorup <krab@trifork.com>
2010-03-02 23:41:42 +0100

    basic tests for == and =:=

---
+++ src/test/erl/equals_tests.erl
@@ -0,0 +1,18 @@
+-module(equals_tests).
+
+-export([test/0]).
+
+test() ->
+    %% Test equals on small integers, large integers, bigints, and non-numbers.
+
+    Operands = [0, 0.0,
+		123, 65536, 123456789, 123456789123456789123456789, 1.0e299,
+		-123, -65536, -123456789, -123456789123456789123456789, -1.0e299,
+                {1,2}, {2,1},
+		atom, [], "abc", "ab", [a|b]],
+
+    [{catch(A==B), catch(B==A),catch(A=:=B), catch(B=:=A), catch(A<B), catch(B<A)}
+            || A <- Operands,
+	       B <- Operands].
+
+
