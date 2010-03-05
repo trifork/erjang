64e400dee581a223b7707260e92d5c479cb9d6fc
Ulrik Skyt <ulrik.skyt@gmail.com>
2010-03-05 01:31:13 +0100

    Created Eclipse OTP.launch target, erl.cmd (for Windows) and changed Compiler to use the pathSeparator for the current platform.

---
+++ erl.cmd
@@ -0,0 +1,12 @@
+@echo off
+
+IF "x%OTPROOT%" == "x" SET OTPROOT="C:\Program Files\erl7.5.7"
+
+java -ea -cp erjang-0.1.jar ^
+    -Derj.threads=1 ^
+    -Derjpath=%OTP_ERTS_EBIN% erjang.OTPMain ^
+    -root %OTPROOT% ^
+    -progname erl ^
+    -- ^
+    -home %HOME% ^
+    %1 %2 %3 %4 %5 %6 %7 %8 %9
