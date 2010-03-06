b81c4aac882dbd2a0593344b656777a0963c5457
Erik Søe Sørensen <erik+gh@nogetmed.dk>
2010-03-06 12:45:44 +0100

    equals_tests: Being more thorough.

--- src/test/erl/equals_tests.erl
+++ src/test/erl/equals_tests.erl
@@ -8,9 +8,31 @@ test() ->
     Operands = [0, 0.0,
 		123, 65536, 123456789, 123456789123456789123456789, 1.0e299,
 		-123, -65536, -123456789, -123456789123456789123456789, -1.0e299,
-                {1,2}, {2,1},
-		atom, [], "abc", "ab", [a|b]],
+		%% Atoms:
+		atom, list_to_atom("another_atom"), list_to_existing_atom("atom"),
+		%% Tuples:
+		{}, {a}, {1,2}, {2,1},
 
-    [{catch(A==B), catch(A=:=B), catch(A<B)}
+		%% Lists:
+		[], [a], [a|b],
+		"ab", "abc", "abc"++[a], ["abc"|a], % EString
+		unicode:characters_to_list(<<"\xC2\xA2">>, utf8), % EBigString
+
+		%% Misc.:
+		self() % EPid
+		%% TODO: Ports?
+	       ],
+
+    {[{catch(A==B), catch(A=:=B), catch(A<B)}
 	     || A <- Operands,
-		B <- Operands].
+		B <- Operands],
+     [{catch(A==copy(A)), catch(A=:=copy(A)), catch(A<copy(A)), catch(A>copy(A))}
+      || A <- Operands]}.
+
+copy(X) when is_atom(X) -> X;
+copy(X) when is_number(X) -> X + 0;
+copy([X|Y]) -> [copy(X) | copy(Y)];
+copy(X) when is_tuple(X) ->
+    list_to_tuple(lists:map(fun copy/1, tuple_to_list(X)));
+copy(<<X, Rest/bitstring>>) -> <<X, (copy(Rest))/bitstring>>;
+copy(X) -> X.
