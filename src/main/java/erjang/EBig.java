1d8efc4a0af6365978b90b594307c7faf5516177
Erik Søe Sørensen <erik+gh@nogetmed.dk>
2010-03-26 13:24:42 +0100

    Handling "EBig bsr EBig" for left operand negative, right operand huge.

--- src/main/java/erjang/EBig.java
+++ src/main/java/erjang/EBig.java
@@ -234,7 +234,10 @@ public final class EBig extends EInteger {
 
 	EInteger r_bsr(BigInteger lhs) {
 		if (BIG_MAX_INT.compareTo(value) < 0) {
-			return new ESmall(0);
+			if (BigInteger.ZERO.compareTo(lhs) > 0)
+				return ESmall.MINUS_ONE;
+			else
+				return ESmall.ZERO;
 		} else {
 			return ERT.box(lhs.shiftRight(value.intValue()));
 		}
