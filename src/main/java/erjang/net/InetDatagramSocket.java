a02f2b626040f73b1d22056739868739deabd485
Kresten Krab Thorup <krab@trifork.com>
2010-04-06 14:27:07 +0200

    BSD/Posix socket abstraction layer; net function is to delay allocating ServerSocket or Socket until we know if the call sequence involves socket.connect() or socket.listen().

---
+++ src/main/java/erjang/net/InetDatagramSocket.java
@@ -0,0 +1,100 @@
+package erjang.net;
+
+import java.io.IOException;
+import java.net.InetSocketAddress;
+import java.net.SocketAddress;
+import java.nio.ByteBuffer;
+import java.nio.channels.DatagramChannel;
+import java.nio.channels.SelectableChannel;
+
+import erjang.driver.efile.Posix;
+
+public class InetDatagramSocket extends InetSocket {
+
+	private DatagramChannel ch;
+
+	public InetDatagramSocket(LazyInetSocket sock) throws IOException {
+		ch = DatagramChannel.open();
+		sock.init(this);
+	}
+
+	@Override
+	public InetSocket accept() throws IOException {
+		throw new erjang.NotImplemented();
+	}
+
+	@Override
+	public void bind(InetSocketAddress localAddress) throws IOException {
+		ch.socket().bind(localAddress);
+	}
+
+	@Override
+	public
+	SelectableChannel channel() {
+		return ch;
+	}
+
+	@Override
+	public boolean connect(InetSocketAddress remote) throws IOException {
+		ch.socket().connect(remote);
+		return true;
+	}
+
+	@Override
+	public boolean finishConnect() throws IOException {
+		// should not happen
+		return true;
+	}
+
+
+	@Override
+	public SocketAddress getLocalSocketAddress() {
+		return ch.socket().getLocalSocketAddress();
+	}
+
+	@Override
+	public boolean isBound() {
+		return ch.socket().isBound();
+	}
+
+	@Override
+	public void listen(int backlog) throws IOException {
+		throw new PosixIOException(Posix.EINVAL, "Cannot listen on UDP socket");
+	}
+
+	@Override
+	public void setReceiveBufferSize(int size) throws IOException {
+		ch.socket().setReceiveBufferSize(size);
+	}
+
+	@Override
+	public void setReuseAddress(boolean on) throws IOException {
+		ch.socket().setReuseAddress(on);
+	}
+
+	@Override
+	public void setSendBufferSize(int size) throws IOException {
+		ch.socket().setSendBufferSize(size);
+	}
+
+	@Override
+	public void setTimeout(int timeout) throws IOException {
+		ch.socket().setSoTimeout(timeout);
+	}
+
+	@Override
+	public void setBroadcast(boolean on) throws IOException {
+		ch.socket().setBroadcast(on);
+	}
+	
+	@Override
+	public void setTrafficClass(int ival) throws IOException {
+		ch.socket().setTrafficClass(ival);
+	}
+	
+	@Override
+	public int send(ByteBuffer packet, SocketAddress target) throws IOException {
+		return ch.send(packet, target);
+	}
+	
+}
---
+++ src/main/java/erjang/net/InetSocket.java
@@ -0,0 +1,141 @@
+package erjang.net;
+
+import java.io.IOException;
+import java.net.InetAddress;
+import java.net.InetSocketAddress;
+import java.net.NetworkInterface;
+import java.net.SocketAddress;
+import java.nio.ByteBuffer;
+import java.nio.channels.SelectableChannel;
+
+import erjang.driver.efile.Posix;
+
+public abstract class InetSocket {
+
+	public static InetSocket open(ProtocolFamily domain, ProtocolType type, Protocol protocol) {
+		return new LazyInetSocket(domain, type, protocol);
+	}
+
+	public abstract SelectableChannel channel();
+
+	public abstract boolean connect(InetSocketAddress remote) throws IOException;
+
+	public abstract void bind(InetSocketAddress localAddress)
+			throws IOException;
+
+	public abstract boolean isBound();
+
+	public abstract void listen(int backlog) throws IOException;
+
+	public abstract InetSocket accept() throws IOException;
+
+	public abstract SocketAddress getLocalSocketAddress();
+
+	public abstract void setReuseAddress(boolean reuse) throws IOException;
+
+	public abstract void setReceiveBufferSize(int size) throws IOException;
+
+	public abstract void setTimeout(int timeout) throws IOException;
+
+	public abstract void setSendBufferSize(int size) throws IOException;
+
+	public void setKeepAlive(boolean on) throws IOException {
+		throw new PosixIOException(Posix.EINVAL,
+				"option SO_KEEPALIVE not supported on "
+						+ this.getClass().getName());
+	}
+
+
+	public  void setOOBInline(boolean on) throws IOException {
+		throw new PosixIOException(Posix.EINVAL,
+				"option SO_OOB_INLINE not supported on "
+						+ this.getClass().getName());
+	}
+
+	public void setTcpNoDelay(boolean on) throws IOException {
+		throw new PosixIOException(Posix.EINVAL,
+				"option SO_TCP_NODELAY not supported on "
+						+ this.getClass().getName());
+	}
+
+	public void setLinger(boolean on, int timeout) throws IOException {
+		throw new PosixIOException(Posix.EINVAL,
+				"option SO_LINGER not supported on "
+						+ this.getClass().getName());
+	}
+
+	public void setBroadcast(boolean on) throws IOException {
+		throw new PosixIOException(Posix.EINVAL,
+				"option SO_BROADCAST not supported on "
+						+ this.getClass().getName());
+	}
+
+	public int send(ByteBuffer packet, SocketAddress target)
+			throws IOException {
+		throw new PosixIOException(Posix.EINVAL,
+				"send(datagram) not supported on "
+						+ this.getClass().getName());
+	}
+
+	public void close() throws IOException {
+		channel().close();
+	}
+
+	public void configureBlocking(boolean block) throws IOException {
+		channel().configureBlocking(block);
+	}
+	
+	public boolean isOpen() {
+		return channel().isOpen();
+	}
+
+	public boolean finishConnect() throws IOException {
+		throw new PosixIOException(Posix.EINVAL,
+				"finishConnect() not supported on "
+						+ this.getClass().getName());
+	}
+
+	public int getLocalPort() {
+		InetSocketAddress addr = (InetSocketAddress) getLocalSocketAddress();
+		if (addr == null) { return 0; }
+		return addr.getPort();
+	}
+
+	public void setTrafficClass(int ival) throws IOException {
+		throw new PosixIOException(Posix.EINVAL,
+				"setTrafficClass() not supported on "
+						+ this.getClass().getName());
+	}
+
+	public void setTimeToLive(int ival) throws IOException {
+		throw new PosixIOException(Posix.EINVAL,
+				"setTimeToLive() not supported on "
+						+ this.getClass().getName());
+	}
+
+	public void setLoopbackMode(boolean on) throws IOException {
+		throw new PosixIOException(Posix.EINVAL,
+				"setLoopbackMode() not supported on "
+						+ this.getClass().getName());
+	}
+
+	public void setInterface(InetAddress addr) throws IOException {
+		throw new PosixIOException(Posix.EINVAL,
+				"setInterface() not supported on "
+						+ this.getClass().getName());
+	}
+
+	public void joinGroup(SocketAddress mcastaddr, NetworkInterface netIf) throws IOException {
+		throw new PosixIOException(Posix.EINVAL,
+				"joinGroup() not supported on "
+						+ this.getClass().getName());
+	}
+
+	public void leaveGroup(SocketAddress mcastaddr, NetworkInterface netIf) throws IOException {
+		throw new PosixIOException(Posix.EINVAL,
+				"leaveGroup() not supported on "
+						+ this.getClass().getName());
+	}
+
+
+}
