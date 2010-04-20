a31e3663a4a35723e234d44cbcc801e3a412ef59
Kresten Krab Thorup <krab@trifork.com>
2010-04-20 02:40:16 +0200

    Assign constants to ProtocolFamily enums

--- src/main/java/erjang/net/ProtocolFamily.java
+++ src/main/java/erjang/net/ProtocolFamily.java
@@ -1,5 +1,16 @@
 package erjang.net;
 
+import erjang.driver.tcp_inet.TCPINet;
+
 public enum ProtocolFamily {
-	INET, INET6, LOOPBACK, ANY
+	INET(TCPINet.INET_AF_INET), 
+	INET6(TCPINet.INET_AF_INET6), 
+	LOOPBACK(TCPINet.INET_AF_LOOPBACK), 
+	ANY(TCPINet.INET_AF_ANY);
+	
+	public final byte code;
+
+	private ProtocolFamily(byte code) {
+		this.code = code;
+	}
 }
