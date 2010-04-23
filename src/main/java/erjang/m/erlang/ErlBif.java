327df173a4ce33692dc90da38d73ec52ee8f5c9b
Kresten Krab Thorup <krab@trifork.com>
2010-04-23 02:12:45 +0200

    Add erlang:date/0 and erlang:time/o

--- src/main/java/erjang/m/erlang/ErlBif.java
+++ src/main/java/erjang/m/erlang/ErlBif.java
@@ -247,9 +247,23 @@ public class ErlBif {
 	}
 
 	@BIF
-	@ErlFun(export = true)
-	static public ETuple3 date() {
-		throw new NotImplemented();
+	static public ETuple date() {
+		GregorianCalendar cal = new GregorianCalendar();
+		int year = cal.get(Calendar.YEAR);
+		int month = cal.get(Calendar.MONTH)+1;
+		int day = cal.get(Calendar.DAY_OF_MONTH);
+		
+		return ETuple.make(ERT.box(year), ERT.box(month), ERT.box(day));
+	}
+
+	@BIF
+	static public ETuple time() {
+		GregorianCalendar cal = new GregorianCalendar();
+		int year = cal.get(Calendar.HOUR_OF_DAY);
+		int month = cal.get(Calendar.MINUTE);
+		int day = cal.get(Calendar.SECOND);
+		
+		return ETuple.make(ERT.box(year), ERT.box(month), ERT.box(day));
 	}
 
 	@BIF
