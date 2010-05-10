1da7b8903b2ef488f953160217d4dca8f293becc
Kresten Krab Thorup <krab@trifork.com>
2010-05-10 12:00:14 +0200

    Add up to arg17 for tail recursive functions (need to make a general solution one of these days...)

--- src/main/java/erjang/EProc.java
+++ src/main/java/erjang/EProc.java
@@ -85,7 +85,9 @@ public final class EProc extends ETask<EInternalPID> {
 	private static final EObject am_noproc = EAtom.intern("noproc");
 
 	public EFun tail;
-	public EObject arg0, arg1, arg2, arg3, arg4, arg5, arg6, arg7, arg8, arg9, arg10, arg11;
+	public EObject arg0, arg1, arg2, arg3, arg4, arg5, 
+				   arg6, arg7, arg8, arg9, arg10, arg11, 
+				   arg12, arg13, arg14, arg15, arg16, arg17;
 	public ErlangException last_exception;
 
 	private EInternalPID self;
