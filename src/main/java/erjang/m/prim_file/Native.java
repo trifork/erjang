bb27f7a51b588a12fb628a20763e658ae356e332
Alessandro Sivieri <alessandro.sivieri@gmail.com>
2010-12-21 23:54:42 +0100

    Added placeholder for the new file.

---
+++ src/main/java/erjang/m/prim_file/Native.java
@@ -0,0 +1,18 @@
+package erjang.m.prim_file;
+
+import erjang.BIF;
+import erjang.ENative;
+import erjang.EObject;
+
+public class Native extends ENative
+{
+	@BIF
+	public static EObject internal_name2native(EObject arg) {
+		return null;
+	}
+	
+	@BIF
+	public static EObject internal_native2name(EObject arg) {
+		return null;
+	}
+}
