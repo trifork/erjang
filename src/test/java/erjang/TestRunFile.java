948e75aa45a376eb71cd6e79945383cd7cf9a35d
Erik Søe Sørensen <eriksoe+gh@gmail.com>
2011-01-11 21:43:21 +0100

    TestRunFile: Converting a binary via String is lossy - at least for some character encodings. Refraining from doing that.

--- src/test/java/erjang/TestRunFile.java
+++ src/test/java/erjang/TestRunFile.java
@@ -133,12 +133,36 @@ public class TestRunFile extends AbstractErjangTestCase {
 
 		byte[] bin = execGetBinaryOutput(cmd);
 
-        //parse out the data part - drop the stdout noise
-        String sbin = new String(bin);
-        int i = sbin.indexOf("DATA::");
-        if (i >= 0) sbin = sbin.substring(i + 6);
+        /* Parse out the data part - drop the stdout noise.
+		 * Search for the substring "DATA::"
+		 * This would be easier if we could could use some
+		 * indexOf()-like function; unfortunately, converting to String
+		 * and back again via getBytes() is lossy in some encodings.
+		 */
+		int offset = 0;
+		int len=bin.length;
+		for (int i=0; i < len-6; i++) {
+			if (bin[i+0]=='D' &&
+				bin[i+1]=='A' &&
+				bin[i+2]=='T' &&
+				bin[i+3]=='A' &&
+				bin[i+4]==':' &&
+				bin[i+5]==':')
+			{
+				offset = i+6;
+				break;
+			}
+		}
 
-		return ErlConvert.binary_to_term(new EBinary((i >= 0) ? sbin.getBytes() : bin));
+		EBinary binOutput = new EBinary(bin, offset, len-offset);
+		try {
+			return ErlConvert.binary_to_term(binOutput);
+		} catch (Throwable e) {
+			System.err.println("Undecodable output: "+e);
+			System.err.println("Output is: "+binOutput);
+			System.err.println("Full output is: "+new EBinary(bin)+"; data offset="+offset);
+			return null;
+		}
 	}
 
 	private void erl_compile(String fileName) throws Exception {
