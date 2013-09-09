58be3e865172544af63b5815f4f4794902624691
Kresten Krab Thorup <krab@trifork.com>
2013-09-09 15:53:21 +0200

    Don't be verbose about __info__ imports missing

--- src/main/java/erjang/EModuleManager.java
+++ src/main/java/erjang/EModuleManager.java
@@ -49,6 +49,7 @@ public class EModuleManager {
 
 	// static FunctionInfo undefined_function = null;
 	static EAtom am_prep_stop = EAtom.intern("prep_stop");
+	static EAtom am___info__ = EAtom.intern("__info__");
 
 	static class FunctionInfo {
 		private final FunID fun;
@@ -136,7 +137,7 @@ public class EModuleManager {
 
 							/** this is just some debugging info to help understand downstream errors */
 							if (get_module_info(fun.module).is_loaded()) {
-								if (fun.function != am_prep_stop)
+								if (fun.function != am_prep_stop && fun.function != am___info__)
 									log.log(Level.INFO, "MISSING "+fun);
 							} else {
 								log.log(Level.FINER, "resolving"+fun);
