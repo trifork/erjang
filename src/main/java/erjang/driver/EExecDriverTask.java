35a6d860deda792a07b770a99a7d9bf08d0f143e
Kresten Krab Thorup <krab@trifork.com>
2013-09-13 14:20:33 +0200

    Fix argument processing for port({spawn, "…"}, …)
    We now actually process quoted arguments, as well
    as escaped quotes within such arguments.

--- src/main/java/erjang/driver/EExecDriverTask.java
+++ src/main/java/erjang/driver/EExecDriverTask.java
@@ -6,7 +6,7 @@
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- *  
+ *
  *     http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
@@ -28,10 +28,11 @@ import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 import java.util.Map;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 import kilim.Pausable;
 import kilim.Task;
-
 import erjang.EAtom;
 import erjang.EBinList;
 import erjang.EBinary;
@@ -53,13 +54,13 @@ import erjang.m.erlang.ErlPort;
 
 /**
  * UNUSED CLASS I KEEP AROUND BECAUSE IT HAS SOME CODE I MAY USE LATER
- * 
+ *
  * A n EPort instance that corresponds to an external executable
  */
 public class EExecDriverTask extends EDriverTask {
 
 	private Process process;
-	
+
 	private DataOutputStream out;
 	private InputStream in;
 
@@ -68,11 +69,11 @@ public class EExecDriverTask extends EDriverTask {
 	private ETuple2 name;
 
 	private EObject command;
-	
+
 	public EObject getName() {
 		return command;
 	}
-	
+
 	/**
 	 * @param owner
 	 * @param name
@@ -80,12 +81,12 @@ public class EExecDriverTask extends EDriverTask {
 	 */
 	public EExecDriverTask(EProc owner, ETuple2 name, EObject command, EObject portSetting) {
 			super(owner.self_handle(), new ExecDriverInstance(name));
-			
+
 			this.command = command;
-			
+
 			// argument can be any list, ... turn it into a string
 			ESeq es = name.elem2.testString();
-			if (es == null) {		
+			if (es == null) {
 				ECons cons;
 				EAtom am;
 				if ((cons = name.elem2.testCons()) != null) {
@@ -96,23 +97,21 @@ public class EExecDriverTask extends EDriverTask {
 					throw ERT.badarg(name, portSetting);
 				}
 			}
-			
+
 			String full_cmd = es.stringValue();
-			
+
 			String[] cmd;
 			if (name.elem1 == ErlPort.am_spawn) {
-				cmd = full_cmd.split(" +");
+				cmd = split_with_quotes(full_cmd);
 			} else {
 				cmd = new String[] { full_cmd };
 			}
-			
+
 			parseOptions(cmd, portSetting);
 
-			File f = ERT.newFile(cmd[0]);
-			if (f.exists() && f.canExecute()) {
-				// ok !
-			} else if (f.isAbsolute()) {
-				throw new ErlangError(EAtom.intern("enoent"));
+			File f = new File(cmd[0]);
+			if (f.isAbsolute()) {
+				// ok //
 			} else {
 
 				String cmd_path = env.get("PATH");
@@ -130,8 +129,16 @@ public class EExecDriverTask extends EDriverTask {
                 }
 			}
 
-			// System.err.println("lauching "+cmd[0]+" "+(f==null?"(not found)":""));		
-			
+			/*
+			System.err.println("** launching ");
+			System.err.println( "  name  = " + name);
+			System.err.println( "  cmd   = " + command);
+			System.err.println( "  opts  = " + portSetting);
+			for (int i = 0; i < cmd.length; i++) {
+				System.err.println(" cmd["+i+"]=" + cmd[i]);
+			}
+			*/
+
 			if (f == null) {
 				throw new ErlangError(EAtom.intern("enoent"));
 			}
@@ -142,7 +149,58 @@ public class EExecDriverTask extends EDriverTask {
 
 
 			super.setupInstance();
-			
+
+	}
+
+	private String[] split_with_quotes(String cmd) {
+		List<String> res = new ArrayList<String>();
+		int length = cmd.length();
+
+
+		for (int i = skip_space(cmd, 0); i < length; i = skip_space(cmd, i)) {
+
+			char ch = cmd.charAt(i);
+			if (ch == '"' || ch == '\'') {
+				i = parse_qarg(i, res, cmd, ch);
+
+			} else {
+
+				int start = i;
+				while (i < length && !Character.isSpaceChar( cmd.charAt(i) )) i++;
+
+				String arg = cmd.substring(start, i);
+				res.add(arg);
+			}
+		}
+
+
+		return res.toArray(new String[res.size()]);
+	}
+
+	private int skip_space(String cmd, int i) {
+		int l = cmd.length();
+		while (i < l && Character.isSpaceChar( cmd.charAt(i) )) i++;
+		return i;
+	}
+
+	int parse_qarg(int i, List<String> args, String cmd, char q) {
+		assert(cmd.charAt(i) == q);
+
+		StringBuffer arg = new StringBuffer();
+		char ch;
+		while (++i < cmd.length() && (ch = cmd.charAt(i)) != q) {
+
+			if (ch == '\\') {
+				i += 1;
+				if (i == cmd.length()) throw ERT.badarg();
+				arg.append( cmd.charAt(i));
+			} else {
+				arg.append( ch );
+			}
+		}
+
+		args.add( arg.toString() );
+		return i+1;
 	}
 
 	/* (non-Javadoc)
