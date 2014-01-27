75fe296e42319f7df098ebd5dfefd94a44d8f9d3
Erik Søe Sørensen <ess@trifork.com>
2014-01-27 13:28:08 +0100

    Make PP of binaries work again: Emulate write_binary_body/2 faithfully.
    This is a replacement for the earlier hack of emulating write_binary/2
    crudely, but we keep the "do not recurse" property.
    
    NB: This introduces an incompatibility with earlier translations of io_lib.
    Flush the jar from the cache...

--- src/main/java/erjang/m/io_lib/Native.java
+++ src/main/java/erjang/m/io_lib/Native.java
@@ -7,52 +7,48 @@ import erjang.ENative;
 import erjang.EObject;
 import erjang.ERT;
 import erjang.EString;
+import erjang.ESeq;
+import erjang.ESmall;
 
 public class Native extends ENative {
 
-	@BIF
-	static public EObject write_binary(EObject arg1, EObject arg2)
-	{
-		EBitString bin = arg1.testBitString();
-		EInteger d = arg2.testInteger();
-		
-		if (bin == null || d == null) 
-			throw ERT.badarg(arg1, arg2);
-		
-		int depth = d.intValue();
-		
-	    StringBuilder sb = new StringBuilder("<<");
-	    long remainingBits = bin.bitSize();
-	    long bitPos = 0L;
-	    while(remainingBits != 0) {
-	        
-	        if (depth == 1) {
-	        	sb.append("..."); 
-	        	break; 
-	        }
-
-	        if (remainingBits > 8) {
-		           int b = bin.intBitsAt( bitPos, 8 );
-		           sb.append( b ).append(',');
-		           bitPos += 8;
-		           remainingBits -= 8;
-		           depth = depth-1;
-		           continue;
-	        }
-
-	        if (remainingBits == 8) {
-	           int b = bin.intBitsAt(bitPos, 8);
-	           sb.append( b );
-	           break;
-	        }
-
-	        int b = bin.intBitsAt(bitPos, (int) remainingBits);
-	        sb.append( b ).append(':').append(remainingBits);
-	        break;
-	     }
-	    
-	    sb.append(">>");
-	    
-	    return new EString ( sb.toString() );
-	}
+    /** Emulate io_lib:write_binary_body(), but without using deep stack. */
+    @BIF
+    static public EObject write_binary_body(EObject arg1, EObject arg2)
+    {
+        EBitString bin = arg1.testBitString();
+        EInteger d = arg2.testInteger();
+        
+        if (bin == null || d == null) 
+            throw ERT.badarg(arg1, arg2);
+        
+        int depth = d.intValue();
+        if (depth < 0) depth=Integer.MAX_VALUE;
+        
+        ESeq builder = ERT.NIL;
+        long bitCount = bin.bitSize();
+        if (bitCount > 8*(depth-1)) {
+            // Replace tail with ellipsis:
+            builder = builder.cons(EString.fromString("..."));
+        } else if (bitCount % 8 > 0) {
+            // Handle tail bits
+            int tailBitCount = (int)(bitCount & 7);
+            int tailBits = bin.intBitsAt(bitCount & (~7), tailBitCount);
+            builder = builder.cons(EString.fromString(String.valueOf(tailBitCount)));
+            builder = builder.cons(ESmall.make(':'));
+            builder = builder.cons(EString.fromString(String.valueOf(tailBits)));
+        }
+        // Handle whole bytes:
+        for (long bitPos=8*(Math.min(bitCount/8, depth-1)-1);
+             bitPos>=0;
+             bitPos-=8)
+        {
+            if (bitPos < bitCount-8) {
+                builder = builder.cons(ESmall.make(','));
+            }
+            String byteAsIntString = String.valueOf(bin.intBitsAt(bitPos, 8));
+            builder = builder.cons(EString.fromString(byteAsIntString));
+        }
+        return builder;
+    }
 }
