4b7b21ee06ccc04aa2a9bf3bf82f1c70a2444a6f
Erik Søe Sørensen <ess@trifork.com>
2014-01-29 14:06:16 +0100

    Organize ETask, preparing for process consistency work.

--- src/main/java/erjang/ETask.java
+++ src/main/java/erjang/ETask.java
@@ -39,25 +39,58 @@ import kilim.Task;
  */
 public abstract class ETask<H extends EHandle> extends kilim.Task {
 
-	static Logger log = Logger.getLogger("erjang.proc");
-	
-	public static final EAtom am_normal = EAtom.intern("normal");
+	private static Logger log = Logger.getLogger("erjang.proc");
+
+    /*==================== Constants =============================*/
+
+    public static final EAtom am_normal = EAtom.intern("normal");
 	protected static final EAtom am_java_exception = EAtom
 			.intern("java_exception");
 	private static final EAtom am_DOWN = EAtom.intern("DOWN");
 	private static final EAtom am_process = EAtom.intern("process");
 
-	/**
+    private static final int MAX_MAILBOX_SIZE = 1000;
+
+    /*==================== Typedefs ==============================*/
+    public enum STATE {
+        INIT, RUNNING, EXIT_SIG, SENDING_EXIT, DONE
+    };
+
+    /*==================== Process state ====================================*/
+
+    protected volatile STATE pstate = STATE.INIT;
+
+    private AtomicReference<PersistentHashSet<EHandle>>
+            linksref = new AtomicReference<PersistentHashSet<EHandle>>(PersistentHashSet.EMPTY);
+
+    protected Map<ERef,EHandle> monitors = new ConcurrentHashMap<ERef, EHandle>();
+
+    // this is not synchronized, as we only mess with it from this proc
+    // TODO That is not accurate, cf. do_proc_termination->send_monitor_exit call. --ESS
+    private Map<ERef,ETuple2> is_monitoring = new HashMap<ERef, ETuple2>();
+
+
+    protected final Mailbox<EObject> mbox = new Mailbox<EObject>(10, MAX_MAILBOX_SIZE);
+
+    /** Reduction counter. */
+    private int reds;
+
+    /** null when task is alive or terminated normally; non-null when task is terminated/terminating abnormally. */
+    protected EObject exit_reason;
+    protected Throwable killer;
+
+    private Mailbox<EObject> print_trace;
+
+    /*==================== Instance methods ====================================*/
+
+    /**
 	 * @return
 	 */
 	public abstract H self_handle();
 
-	private AtomicReference<PersistentHashSet<EHandle>> 
-		linksref = new AtomicReference<PersistentHashSet<EHandle>>(PersistentHashSet.EMPTY);
+    /*--------- Link related ----------------------------*/
 
-	protected Map<ERef,EHandle> monitors = new ConcurrentHashMap<ERef, EHandle>();
-
-	public void unlink(EHandle other) throws Pausable {
+    public void unlink(EHandle other) throws Pausable {
 		unlink_oneway(other);
 		other.unlink_oneway(self_handle());
 	}
@@ -80,10 +113,10 @@ public abstract class ETask<H extends EHandle> extends kilim.Task {
 		IPersistentSet<EHandle> links = linksref.get();
 		return links.count() == 0;
 	}
-	
+
 	/**
 	 * @param task
-	 * @throws Pausable 
+	 * @throws Pausable
 	 */
 	public void link_to(ETask<?> task) throws Pausable {
 		link_to(task.self_handle());
@@ -94,25 +127,25 @@ public abstract class ETask<H extends EHandle> extends kilim.Task {
           handle.link_oneway((EHandle) self_handle()); // Ignore failure if linker has received SIG_EXIT
         } else {
 			link_failure(handle);
-		}		
+		}
 	}
 
 	public boolean link_oneway(EHandle h) {
 		// TODO: check if h is valid.
-		
+
 		if (h.exists()) {
 
 			PersistentHashSet old, links;
 			try {
-				
+
 				do {
 					old = linksref.get();
 					links = (PersistentHashSet) old.cons(h);
 				} while (!linksref.weakCompareAndSet(old, links));
-				
+
 			} catch (Exception e) {
 				throw new RuntimeException(e);
-			}		
+			}
 
 			return true;
 		} else {
@@ -128,7 +161,6 @@ public abstract class ETask<H extends EHandle> extends kilim.Task {
 		return res;
 	}
 
-
 	/**
 	 * @param h
 	 * @throws Pausable 
@@ -140,7 +172,131 @@ public abstract class ETask<H extends EHandle> extends kilim.Task {
 		throw new ErlangError(ERT.am_noproc);
 	}
 
-	@SuppressWarnings("unchecked")
+    /*--------- Monitor related -------------------------*/
+    /**
+     * @param object
+     * @param ref TODO
+     * @param selfHandle
+     * @return
+     * @throws Pausable
+     */
+    public boolean monitor(EHandle observed, EObject object, ERef ref) throws Pausable {
+        if (!observed.add_monitor(self_handle(), ref)) {
+            //	System.err.println("unable to add monitor to self="+self_handle()+" pid="+observed+" ref="+ref);
+            return false;
+        }
+        this.is_monitoring.put(ref, new ETuple2(observed, object));
+        return true;
+    }
+
+    public boolean monitor(EObject object, ERef ref) throws Pausable {
+        this.is_monitoring.put(ref, new ETuple2(object, object));
+        return true;
+    }
+
+    /**
+     * @param r
+     * @return
+     * @throws Pausable
+     */
+    public EObject demonitor(ERef r) throws Pausable {
+        ETuple2 pair = is_monitoring.remove(r);
+        if (pair == null) {
+            return null;
+        }
+
+        return pair.elem1;
+    }
+
+    /**
+     * @param ref TODO
+     * @param self2
+     * @return
+     */
+    public boolean add_monitor(EHandle target, ERef ref) {
+        monitors.put(ref, target);
+        return true;
+    }
+
+    /**
+     * @param r
+     */
+    public void remove_monitor(ERef r, boolean flush) {
+        EHandle val = monitors.remove(r);
+        if (flush) {
+            // TODO: do we need to represent flush somehow?
+        }
+    }
+
+    public EHandle get_monitored_process(ERef monitor) {
+        ETuple2 tup = is_monitoring.get(monitor);
+        if (tup == null) return null;
+        return tup.elem1.testHandle();
+    }
+
+    public EObject get_monitored_object(ERef monitor) {
+        ETuple2 tup = is_monitoring.get(monitor);
+        return tup.elem2;
+    }
+
+
+    /*--------- Process state ---------------------------*/
+
+    /**
+     * @return
+     */
+    public boolean exists() {
+        STATE ps = pstate; // Read volatile just once!
+        return ps == STATE.INIT || ps == STATE.RUNNING;
+    }
+
+    /*--------- Termination related ---------------------*/
+    /* (non-Javadoc)
+	 * @see kilim.Task#checkKill()
+	 */
+    @Override
+    public void checkKill() {
+        check_exit();
+    }
+
+    /**
+     * will check if this process have received an exit signal (and we're not
+     * trapping)
+     */
+    public final void check_exit() {
+        if (this.pstate == STATE.EXIT_SIG) {
+            do_check_exit();
+        }
+
+        if (print_trace != null) {
+            Mailbox<EObject> mb = print_trace;
+            if (mb != null) {
+
+                H handle = self_handle();
+                ESeq trace = new ErlangError(ERT.am_ok).getTrace();
+                mb.putb(new ETuple2(handle, trace));
+
+            }
+            print_trace = null;
+        }
+    }
+
+
+    /** because the above is called a bazillion times, we split this out to make it easier to inline */
+    private void do_check_exit() throws ErlangExitSignal {
+        if (exit_reason != null) {
+            this.pstate = STATE.SENDING_EXIT;
+            EObject reason = exit_reason;
+            //System.err.println("---- [" + killer + "]");
+            ErlangExitSignal e = new ErlangExitSignal(reason, killer);
+            //e.printStackTrace();
+            //System.err.println("---- [ ... ]");
+            throw e;
+        }
+
+    }
+
+    @SuppressWarnings("unchecked")
 	protected void do_proc_termination(EObject exit_reason) throws Pausable {
 		this.exit_reason = exit_reason;
 		H me = self_handle();
@@ -176,99 +332,18 @@ public abstract class ETask<H extends EHandle> extends kilim.Task {
 			mbox_send(ETuple.make(am_DOWN, ref, am_process, pair.elem2, reason));
 		}
 	}
-	
-	
-
-	// this is not synchronized, as we only mess with it from this proc
-	Map<ERef,ETuple2> is_monitoring = new HashMap<ERef, ETuple2>();
-
-	/**
-	 * @param object
-	 * @param ref TODO
-	 * @param selfHandle
-	 * @return
-	 * @throws Pausable 
-	 */
-	public boolean monitor(EHandle observed, EObject object, ERef ref) throws Pausable {
-		if (!observed.add_monitor(self_handle(), ref)) {
-		//	System.err.println("unable to add monitor to self="+self_handle()+" pid="+observed+" ref="+ref);
-			return false;
-		}
-		this.is_monitoring.put(ref, new ETuple2(observed, object));
-		return true;
-	}
-
-	public boolean monitor(EObject object, ERef ref) throws Pausable {
-		this.is_monitoring.put(ref, new ETuple2(object, object));
-		return true;
-	}
-
-	/**
-	 * @param r
-	 * @return
-	 * @throws Pausable 
-	 */
-	public EObject demonitor(ERef r) throws Pausable {
-		ETuple2 pair = is_monitoring.remove(r);
-		if (pair == null) {
-			return null;
-		}
-		
-		return pair.elem1;
-	}
-	
-	/**
-	 * @param ref TODO
-	 * @param self2
-	 * @return
-	 */
-	public boolean add_monitor(EHandle target, ERef ref) {
-		monitors.put(ref, target);
-		return true;
-	}
-	
-	/**
-	 * @param r
-	 */
-	public void remove_monitor(ERef r, boolean flush) {
-		EHandle val = monitors.remove(r);
-		if (flush) {
-			// TODO: do we need to represent flush somehow?
-		}
-	}
-	
-	public EHandle get_monitored_process(ERef monitor) {
-		ETuple2 tup = is_monitoring.get(monitor);
-		if (tup == null) return null;
-		return tup.elem1.testHandle();
-	}
-
-	public EObject get_monitored_object(ERef monitor) {
-		ETuple2 tup = is_monitoring.get(monitor);
-		return tup.elem2;
-	}
 
 
+    /*--------- Mbox related ----------------------------*/
 
+    /**
+     * @return
+     */
+    public Mailbox<EObject> mbox() {
+        return mbox;
+    }
 
-	static final int MAX_MAILBOX_SIZE = 1000;
-	protected final Mailbox<EObject> mbox = new Mailbox<EObject>(10, MAX_MAILBOX_SIZE);
-
-	public enum STATE {
-		INIT, RUNNING, EXIT_SIG, SENDING_EXIT, DONE
-	};
-
-	protected volatile STATE  pstate = STATE.INIT;
-	protected EObject exit_reason;
-
-    /** Reduction counter. */
-	private int reds;
-
-	protected Throwable killer;
-
-	private Mailbox<EObject> print_trace;
-
-	/**
+    /**
 	 * @throws Pausable
 	 * 
 	 */
@@ -298,17 +373,6 @@ public abstract class ETask<H extends EHandle> extends kilim.Task {
 	public void mbox_remove_one() throws Pausable {
 		mbox.get();
 	}
-
-    public int get_reductions() { return reds;}
-
-    public void bump_reductions(int amount) throws Pausable {
-        reds += amount;
-        if (reds > 1000) {
-            reds = 0;
-            Task.yield();
-        }
-
-    }
 
 	/**
 	 * @param from
@@ -355,72 +419,24 @@ public abstract class ETask<H extends EHandle> extends kilim.Task {
 	protected abstract void process_incoming_exit(EHandle from, EObject reason, boolean is_erlang_exit2) throws Pausable
 			;
 
-	/**
-	 * will check if this process have received an exit signal (and we're not
-	 * trapping)
-	 */
-	public final void check_exit() {
-		if (this.pstate == STATE.EXIT_SIG) {
-			do_check_exit();
-		}
+    /*--------- Reduction count ------------------------*/
 
-		if (print_trace != null) {
-			Mailbox<EObject> mb = print_trace;
-			if (mb != null) {
-								
-				H handle = self_handle();
-				ESeq trace = new ErlangError(ERT.am_ok).getTrace(); 
-				mb.putb(new ETuple2(handle, trace));
-								
-			}
-			print_trace = null;
-		}
-	}
+    public int get_reductions() { return reds;}
 
+    public void bump_reductions(int amount) throws Pausable {
+        reds += amount;
+        if (reds > 1000) {
+            reds = 0;
+            Task.yield();
+        }
 
-	public void printStackTrace(Mailbox<EObject> info) {
-		this.print_trace = info;
-		this.resume();
-	}
+    }
 
+    /*--------- Miscellaneous ---------------------------*/
 
-	
-	/** because the above is called a bazillion times, we split this out to make it easier to inline */
-	private void do_check_exit() throws ErlangExitSignal {
-		if (exit_reason != null) {
-			this.pstate = STATE.SENDING_EXIT;
-			EObject reason = exit_reason;
-			//System.err.println("---- [" + killer + "]");
-			ErlangExitSignal e = new ErlangExitSignal(reason, killer);
-			//e.printStackTrace();
-			//System.err.println("---- [ ... ]");
-			throw e;
-		}
-		
-	}
-	
-
-	/* (non-Javadoc)
-	 * @see kilim.Task#checkKill()
-	 */
-	@Override
-	public void checkKill() {
-		check_exit();
-	}
-
-	/**
-	 * @return
-	 */
-	public Mailbox<EObject> mbox() {
-		return mbox;
-	}
-
-	/**
-	 * @return
-     */
-    public boolean exists() {
-      STATE ps = pstate; // Read volatile just once!
-      return ps == STATE.INIT || ps == STATE.RUNNING;
+    public void printStackTrace(Mailbox<EObject> info) {
+        this.print_trace = info;
+        this.resume();
     }
 
 }
